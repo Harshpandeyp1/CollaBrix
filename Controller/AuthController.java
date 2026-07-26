b083cbe518c0a5dc409110761224212062d4f05d
Harsh pandey <hk9976057@gmail.com>
2026-07-26 21:48:57 +0530

    Add files via upload

---
+++ Controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.collabrix.backend.Controller;
+
+import com.example.collabrix.backend.Dto.AuthResponse;
+import com.example.collabrix.backend.Dto.LoginRequest;
+import com.example.collabrix.backend.Dto.RegisterRequest;
+import com.example.collabrix.backend.Service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping({"/auth", "/api/auth"})
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse>register(
+            @Valid @RequestBody RegisterRequest request
+            ){
+        return ResponseEntity.ok(
+                authService.register(request)
+        );
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(
+                authService.login(request)
+        );
+    }
+}

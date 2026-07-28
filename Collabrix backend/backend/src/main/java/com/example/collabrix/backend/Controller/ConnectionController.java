2fbfa53037386f9765b1fa07e656a717d4e0afce
Harsh pandey <hk9976057@gmail.com>
2026-07-28 23:25:55 +0530

    Add files via upload

---
+++ Collabrix backend/backend/src/main/java/com/example/collabrix/backend/Controller/ConnectionController.java
@@ -0,0 +1,28 @@
+package com.example.collabrix.backend.Controller;
+
+import com.example.collabrix.backend.Dto.ConnectionRequestDto;
+import com.example.collabrix.backend.Service.ConnectionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping({"/connections", "/api/connections"})
+@RequiredArgsConstructor
+public class ConnectionController {
+    private final ConnectionService connectionService;
+
+
+    @PostMapping("/request")
+    public String sendConnectionRequest(
+            @RequestBody ConnectionRequestDto requestDto,
+             Authentication authentication
+    ){
+        String senderEmail = authentication.getName();
+        connectionService.sendConnectionRequest(
+                senderEmail,
+                requestDto.getReceiverId()
+        );
+        return "connection request sent";
+    }
+}
---
+++ Collabrix backend/backend/src/main/java/com/example/collabrix/backend/Controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.collabrix.backend.Controller;
+
+import com.example.collabrix.backend.Dto.UserSuggestionDto;
+import com.example.collabrix.backend.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("suggestion")
+    public List<UserSuggestionDto> getPeopleYouMayKnow(
+            Authentication authentication
+    ) {
+        return userService.getPeopleYouMayKnow(authentication.getName());
+    }
+}

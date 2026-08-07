a30c65ffe7d91e2a55bc36ad319ea314bd6d96d5
Harsh pandey <hk9976057@gmail.com>
2026-08-07 23:04:45 +0530

    Add files via upload

---
+++ collabrix frontend/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Login from './Pages/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './Pages/Landing'
+import './App.css'
+import Mainpage from './Pages/Mainpage'
+import ProfilePage from './Pages/Profilepage'
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/mainpage" element={<Mainpage />} />
+      <Route path="/Profile" element={<ProfilePage />} />
+    </Routes>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App
+

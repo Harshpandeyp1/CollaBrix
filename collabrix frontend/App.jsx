190390aca75a1ed88ec4f23296952be10cb1b9f0
Harsh pandey <hk9976057@gmail.com>
2026-08-22 23:30:45 +0530

    Add files via upload

--- collabrix frontend/App.jsx
+++ collabrix frontend/App.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import Login from './Pages/Login'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Landing from './Pages/Landing'
 import './App.css'
 import Mainpage from './Pages/Mainpage'
 import ProfilePage from './Pages/Profilepage'
+import Connection from './Pages/Connection'
+import Notifications from './Pages/Notifications'
+import Project from './Pages/Project'
 function App() {
-  
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
 
   return (
+      <div className="min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-white transition-colors">
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Landing />} />
+    {/* your application */}
+     <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/mainpage" element={<Mainpage />} />
       <Route path="/Profile" element={<ProfilePage />} />
-    </Routes>
+      <Route path="/Connection" element={<Connection />} />
+      <Route path="/notification" element={<Navigate to="/notifications" replace />} />
+      <Route path="/notifications" element={<Notifications />} />
+      <Route path="/Project" element={<Project />} />
+      </Routes>
     
     </BrowserRouter>
+     </div>
+     
+    
   )
 }
 
